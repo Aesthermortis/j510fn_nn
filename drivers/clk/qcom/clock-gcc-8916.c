016fe5322964b361cc10760bba122eb1b9476da6
maingocvinh <97122860+maingocvinh@users.noreply.github.com>
2022-08-26 11:48:49 +0700

    GPU: Increase frequency

--- drivers/clk/qcom/clock-gcc-8916.c
+++ drivers/clk/qcom/clock-gcc-8916.c
@@ -551,7 +551,7 @@ static struct clk_freq_tbl ftbl_gcc_camss_vfe0_clk[] = {
 	F( 200000000,	   gpll0,   4,	  0,	0),
 	F( 266670000,	   gpll0,   3,	  0,	0),
 	F( 320000000,	   gpll0, 2.5,	  0,	0),
-	F( 400000000,	   gpll0,   2,	  0,	0),
+	F( 410000000,	   gpll0,   2,	  0,	0),
 	F( 465000000,	   gpll2,   2,	  0,	0),
 	F_END
 };
@@ -581,8 +581,8 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_465_clk[] = {
 	F( 200000000,  gpll0_aux,   4,	  0,	0),
 	F( 266670000,  gpll0_aux,   3,	  0,	0),
 	F( 294912000,	   gpll1,   3,	  0,	0),
-	F( 310000000,	   gpll2,   3,	  0,	0),
-	F( 400000000,  gpll0_aux,   2,	  0,	0),
+	F( 333333000,	   gpll2,   3,	  0,	0),
+	F( 410000000,  gpll0_aux,   2,	  0,	0),
 	F( 465000000,      gpll2,   2,	  0,	0),
 	F_END
 };
@@ -597,8 +597,8 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk[] = {
 	F( 200000000,  gpll0_aux,   4,	  0,	0),
 	F( 266670000,  gpll0_aux,   3,	  0,	0),
 	F( 294912000,	   gpll1,   3,	  0,	0),
-	F( 310000000,	   gpll2,   3,	  0,	0),
-	F( 400000000,  gpll0_aux,   2,	  0,	0),
+	F( 333333000,	   gpll2,   3,	  0,	0),
+	F( 410000000,  gpll0_aux,   2,	  0,	0),
 	F_END
 };
 
@@ -612,7 +612,7 @@ static struct rcg_clk gfx3d_clk_src = {
 		.dbg_name = "gfx3d_clk_src",
 		.ops = &clk_ops_rcg,
 		VDD_DIG_FMAX_MAP3(LOW, 200000000, NOMINAL, 310000000, HIGH,
-			400000000),
+			410000000),
 		CLK_INIT(gfx3d_clk_src.c),
 	},
 };
